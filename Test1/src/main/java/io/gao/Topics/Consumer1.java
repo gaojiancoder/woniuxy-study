69a3176f585a1241c7c26aa5ffef8906c80be157
gaojian <gaojian@192.168.0.101>
2023-01-27 16:25:32 +0800

    练习topics模式 创建两个队列 其中一个队列绑定两个路由key 采用*.和#.通配符

---
+++ Test1/src/main/java/io/gao/Topics/Consumer1.java
@@ -0,0 +1,29 @@
+package io.gao.Topics;
+
+import com.rabbitmq.client.*;
+import io.gao.Utils.RabbitmqUtils;
+
+public class Consumer1 {
+
+    public static final String Exchange_name = "topic_test";
+
+    public static void main(String[] args) throws Exception {
+
+        //获取信道
+        Channel channel = RabbitmqUtils.getChannel();
+
+        //创建交换机
+        channel.exchangeDeclare(Exchange_name, BuiltinExchangeType.TOPIC);
+        //创建队列
+        channel.queueDeclare("topic01", false, false, false, null);
+        //绑定队列和路由key
+        channel.queueBind("topic01", Exchange_name, "*.orange.*");
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println("*。orange。*接收到的消息" + message.getBody());
+        };
+        CancelCallback cancelCallback = consumerTag -> {
+        };
+        channel.basicConsume("topic01", true, deliverCallback, cancelCallback);
+    }
+}
---
+++ Test1/src/main/java/io/gao/Topics/Consumer3.java
@@ -0,0 +1,32 @@
+package io.gao.Topics;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import io.gao.Utils.RabbitmqUtils;
+
+public class Consumer3 {
+
+    public static final String Exchange_name = "topic_test";
+
+    public static void main(String[] args) throws Exception {
+
+        //获取信道
+        Channel channel = RabbitmqUtils.getChannel();
+
+        //创建交换机
+        channel.exchangeDeclare(Exchange_name, BuiltinExchangeType.TOPIC);
+        //创建队列
+        channel.queueDeclare("topic02", false, false, false, null);
+        //绑定队列和路由key
+        channel.queueBind("topic02", Exchange_name, "lazy.#");
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println("lazy接收到的消息" + message.getBody());
+        };
+        CancelCallback cancelCallback = consumerTag -> {
+        };
+        channel.basicConsume("topic02", true, deliverCallback, cancelCallback);
+    }
+}
